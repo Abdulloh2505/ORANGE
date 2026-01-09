beea65d92c864a7a81560c61c69b4f67cce037f0
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-01-09 09:51:15 +0900

    feat: creat project standarts

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
-import  MemberService from "../models/Member.service";
+import MemberService from "../models/Member.service";
 
 
 
@@ -8,7 +8,8 @@ import  MemberService from "../models/Member.service";
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
-    res.send("Home Page");
+        console.log('goHome');
+        res.send("Home Page");
     } catch (err) {
         console.log("Error, goHome:", err);
     }
@@ -16,7 +17,8 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
     try {
-    res.send("Login Page");
+        console.log('getLogin');
+        res.send("Login Page");
     } catch (err) {
         console.log("Error, goLogin:", err);
     }
@@ -24,7 +26,8 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
-    res.send("Signup Page");
+        console.log('getSignup');
+        res.send("Signup Page");
     } catch (err) {
         console.log("Error, goSignup:", err);
     }
