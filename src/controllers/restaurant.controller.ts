0109c9aae3c2e60fdc8d566922f69155dd11fccf
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-01-09 12:19:55 +0900

    feat:create restoran member related get and post

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -30,8 +30,26 @@ restaurantController.getSignup = (req: Request, res: Response) => {
         res.send("Signup Page");
     } catch (err) {
         console.log("Error, goSignup:", err);
-    }
-};
+    }}
+
+    restaurantController.processLogin = (req: Request, res: Response) => {
+    try {
+        console.log('processLogin');
+        res.send("DONE");
+    } catch (err) {
+        console.log("Error, processLogin:", err);
+    }}
+
+
+      restaurantController.processSignup = (req: Request, res: Response) => {
+    try {
+        console.log('processSignup');
+        res.send("DONE");
+    } catch (err) {
+        console.log("Error, processSignup:", err);
+    }};
+
+
 
 
 export default restaurantController;
