32c1fd91d9d9d7eba15584086ccb1bf72e3ca242
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-01-09 09:28:10 +0900

    feat: develop member schima model

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,14 @@
+ export enum MemberType {
+    USER = "USER",
+    ORANGECAFE = "ORANGECAFE",
+}
+
+
+export enum MemberStatus {
+    ACTIVE = "ACTIVE",
+    BLOCK = "BLOCK",
+    DELETE = "DELETE",
+}
+
+
+
