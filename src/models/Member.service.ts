792994019371cab1bbf29c7e48b73a7027185807
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-01-11 07:30:30 +0900

    feat: develop restaurant login process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,5 +1,5 @@
 import MemberModel from "../schema/Member.model";
-import { Member, MemberInput } from "../libs/types/member";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors from "../libs/Error";
 import { HttpCode } from "../libs/Error";
 import { Message } from "../libs/Error";
@@ -26,6 +26,31 @@ import { MemberType } from "../libs/enums/member.enum";
     } catch (err) {
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     }
+  }
+
+
+  public async processLogin(input: LoginInput): Promise<Member> {
+    const member = await this.memberModel
+      .findOne(
+        { memberNick: input.memberNick },
+        { memberNick: 1, memberPassword: 1 }
+      )
+      .exec();
+
+    if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+   // const isMatch = await bcrypt.compare(
+      input.memberPassword,
+      member.memberPassword
+   
+
+//   );
+     const isMatch = input.memberPassword === member.memberPassword;
+
+    if (!isMatch) {
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+    }
+    return await this.memberModel.findById(member._id).exec();
   }
  }
 
